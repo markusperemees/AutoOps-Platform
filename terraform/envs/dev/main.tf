2df4e60326f51810c4049a142bdabc4f591416fe
Markus Peremees <mperem@taltech.ee>
2026-03-11 12:44:03 +0200

    style(terraform): run fmt for envs/dev main module wiring

--- terraform/envs/dev/main.tf
+++ terraform/envs/dev/main.tf
@@ -11,10 +11,10 @@ module "vpc" {
 module "security" {
   source = "../../modules/security"
 
-  project_name        = var.project_name
-  environment         = var.environment
-  vpc_id              = module.vpc.vpc_id
-  ssh_cidr            = var.ssh_cidr
+  project_name = var.project_name
+  environment  = var.environment
+  vpc_id       = module.vpc.vpc_id
+  ssh_cidr     = var.ssh_cidr
 }
 
 module "ec2" {
