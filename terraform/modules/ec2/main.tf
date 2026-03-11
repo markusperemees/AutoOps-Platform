4b095ca2549ebda203fe56ffdd1476c28d2f3c76
Markus Peremees <mperem@taltech.ee>
2026-03-11 13:11:30 +0200

    fix(terraform): resolve AMI from SSM parameter in EC2 module for CI/CD apply

--- terraform/modules/ec2/main.tf
+++ terraform/modules/ec2/main.tf
@@ -1,5 +1,23 @@
+locals {
+  is_monitoring_ami_id = can(regex("^ami-[a-z0-9]+$", var.monitoring_instance_ami))
+  is_app_ami_id        = can(regex("^ami-[a-z0-9]+$", var.app_instance_ami))
+
+  monitoring_ami_parameter_name = replace(var.monitoring_instance_ami, "resolve:ssm:", "")
+  app_ami_parameter_name        = replace(var.app_instance_ami, "resolve:ssm:", "")
+}
+
+data "aws_ssm_parameter" "monitoring_ami" {
+  count = local.is_monitoring_ami_id ? 0 : 1
+  name  = local.monitoring_ami_parameter_name
+}
+
+data "aws_ssm_parameter" "app_ami" {
+  count = local.is_app_ami_id ? 0 : 1
+  name  = local.app_ami_parameter_name
+}
+
 resource "aws_instance" "monitoring_instance" {
-  ami                         = var.monitoring_instance_ami
+  ami                         = local.is_monitoring_ami_id ? var.monitoring_instance_ami : data.aws_ssm_parameter.monitoring_ami[0].value
   instance_type               = var.monitoring_instance_type
   subnet_id                   = var.public_subnet_id
   vpc_security_group_ids      = [var.monitoring_sg_id]
@@ -22,9 +40,9 @@ resource "aws_instance" "monitoring_instance" {
 }
 
 resource "aws_instance" "app_instance" {
-  count                       = var.app_instance_count
+  count = var.app_instance_count
 
-  ami                         = var.app_instance_ami
+  ami                         = local.is_app_ami_id ? var.app_instance_ami : data.aws_ssm_parameter.app_ami[0].value
   instance_type               = var.app_instance_type
   subnet_id                   = var.private_subnet_id
   vpc_security_group_ids      = [var.app_sg_id]
