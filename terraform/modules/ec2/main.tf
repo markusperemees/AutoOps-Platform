1dee586afdb103f158466c147acd60b564822158
Markus Peremees <mperem@taltech.ee>
2026-03-11 13:25:13 +0200

    refactor(terraform): simplify EC2 AMI handling to direct ami IDs

--- terraform/modules/ec2/main.tf
+++ terraform/modules/ec2/main.tf
@@ -1,23 +1,5 @@
-locals {
-  is_monitoring_ami_id = can(regex("^ami-[a-z0-9]+$", var.monitoring_instance_ami))
-  is_app_ami_id        = can(regex("^ami-[a-z0-9]+$", var.app_instance_ami))
-
-  monitoring_ami_parameter_name = replace(var.monitoring_instance_ami, "resolve:ssm:", "")
-  app_ami_parameter_name        = replace(var.app_instance_ami, "resolve:ssm:", "")
-}
-
-data "aws_ssm_parameter" "monitoring_ami" {
-  count = local.is_monitoring_ami_id ? 0 : 1
-  name  = local.monitoring_ami_parameter_name
-}
-
-data "aws_ssm_parameter" "app_ami" {
-  count = local.is_app_ami_id ? 0 : 1
-  name  = local.app_ami_parameter_name
-}
-
 resource "aws_instance" "monitoring_instance" {
-  ami                         = local.is_monitoring_ami_id ? var.monitoring_instance_ami : data.aws_ssm_parameter.monitoring_ami[0].value
+  ami                         = var.monitoring_instance_ami
   instance_type               = var.monitoring_instance_type
   subnet_id                   = var.public_subnet_id
   vpc_security_group_ids      = [var.monitoring_sg_id]
@@ -42,7 +24,7 @@ resource "aws_instance" "monitoring_instance" {
 resource "aws_instance" "app_instance" {
   count = var.app_instance_count
 
-  ami                         = local.is_app_ami_id ? var.app_instance_ami : data.aws_ssm_parameter.app_ami[0].value
+  ami                         = var.app_instance_ami
   instance_type               = var.app_instance_type
   subnet_id                   = var.private_subnet_id
   vpc_security_group_ids      = [var.app_sg_id]
